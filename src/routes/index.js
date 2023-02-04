5098a468c6b0d3b2d475349444a010cfccd678fd
fredyxander <freddy5210@gmail.com>
2023-02-04 11:47:40 -0500

    mvc api backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import * as UserController from "../controllers/user.controller.js";
+
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.render("html-onwire")
-});
+router.get("/users",UserController.getUsersController);
+router.post("/user",UserController.saveUserController);
 
 export {router as apiRouter};
--- src/server.js
+++ src/server.js
@@ -9,12 +9,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname+"/public"));
-
-//config motor de plantillas
-app.engine('hbs', handlebars.engine({ extname: '.hbs'}));
-app.set('views', __dirname+'/views');
-app.set('view engine', 'hbs');
 
 //routes
 app.use(apiRouter);
